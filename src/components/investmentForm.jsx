4fb7f22dcdd1a004f7b5ac308a0461e5b0cc6b9c
GuilhermeRenno <guilherme.silveirareno@gmail.com>
2025-06-07 22:06:29 -0300

    Refazendo HTML e CSS para o forms de investimento e listas, e implementando um design responsivo

--- src/components/investmentForm.jsx
+++ src/components/investmentForm.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, } from "react";
+import './investmentForm.css'; 
 
 function InvestmentForm({onSubmit, onUpdate,onCancel, editingInvestment}) {
 
--- src/components/investmentList.jsx
+++ src/components/investmentList.jsx
@@ -1,3 +1,6 @@
+
+import './investmentList.css';
+
 const formatDate = (date) => {
   const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
   return new Date(date).toLocaleDateString('pt-BR', options);
@@ -14,6 +17,7 @@ function InvestmentList({investments, onDelete, onEdit}) {
     return(
 
         <div className="investment-list">
+            <div className="table-container">
             <table>
                 <thead>
                     <tr>
@@ -27,18 +31,19 @@ function InvestmentList({investments, onDelete, onEdit}) {
                 <tbody>
                    {investments.map((inv) => (
                         <tr key={inv.id}>
-                            <td>{inv.nome}</td>
-                            <td>{inv.tipo}</td>
-                            <td>{formatCurrency(inv.valor)}</td>
-                            <td>{formatDate(inv.data)}</td>
-                            <td>
-                                <button className="edit-btn" onClick={() => onEdit(inv)}>Editar</button>
-                                <button className="delete-btn" onClick={()=> onDelete(inv.id)}>Excluir</button>
-                            </td>
+                        <td data-label="Nome">{inv.nome}</td>
+                        <td data-label="Tipo">{inv.tipo}</td>
+                        <td data-label="Valor Investido">{formatCurrency(inv.valor)}</td>
+                        <td data-label="Data">{formatDate(inv.data)}</td>
+                        <td data-label="Ações">
+                            <button className="edit-btn" onClick={() => onEdit(inv)}>Editar</button>
+                            <button className="delete-btn" onClick={() => onDelete(inv.id)}>Excluir</button>
+                        </td>
                         </tr>
                      ))}
                 </tbody>
             </table>
+            </div>
         </div>
     );
 }
